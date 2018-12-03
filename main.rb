332bfe6ce0ddc03e7719e407131e47a869aa589d
Jayden Shaw <jayden.shaw@gmail.com>
2018-12-03 23:49:07 +0000

    Game logic completed.

--- main.rb
+++ main.rb
@@ -17,7 +17,7 @@ def question
     @num1 = rand(1..20)
     @num2 = rand(1..20)
     @cor_ans = @num1 + @num2
-    @quest_num = +1
+    @quest_num += 1
     if @quest_num.even?
         puts "#{@p2}'s Turn"
     else 
@@ -27,16 +27,44 @@ def question
     print ">"
     quest_ans = $stdin.gets.chomp.to_i
     if @cor_ans == quest_ans
-        puts "Correct"
+        puts "Correct!"
+        puts "----------"
+        sleep(1)
         tally
     else 
+        @wrong = true
         puts "Oh boy... Correct answer is #{@num1 + @num2}"
+        puts "----------"
+        sleep(1)
         tally
     end
 end
 
+# Count Player Lives
 def tally
-    
+    if @wrong == true && @quest_num.even?
+        @p2_lives -= 1
+        puts "#{@p2} has #{@p2_lives} lives remaining"
+        puts "----------"
+    end
+    if @wrong == true && @quest_num.odd?
+        @p1_lives -= 1
+        puts "#{@p1} has #{@p1_lives} lives remaining"
+        puts "----------"
+    end
+    if @p2_lives == 0
+        puts "#{@p2} has no lives remaining."
+        puts "#{@p1} wins!"
+    end
+    if @p1_lives == 0
+        puts "#{@p1} has no lives remaining."
+        puts "#{@p2} wins!"
+    else
+        puts "Next Question"
+        puts "----------"
+        sleep(1)
+        question
+    end
 end
 
 # Initialize - Players Names - Ready to Start?
